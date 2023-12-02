2f36aa7c2074ce95fdbba1928e1218cac126044b
adnanjpg <adnanfahedhr@gmail.com>
2023-12-02 17:29:31 +0300

    feat: add string case converter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,27 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,15 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -59,67 +35,128 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(),
+      body: const Center(
+        child: StringCaseCoverter(),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+class StringCaseCoverter extends StatefulWidget {
+  const StringCaseCoverter({super.key});
+
+  @override
+  State<StringCaseCoverter> createState() => _StringCaseCoverterState();
+}
+
+enum _Case {
+  upper,
+  lower,
+  camel,
+  snake;
+}
+
+class _StringCaseCoverterState extends State<StringCaseCoverter> {
+  String _input = '';
+  String _output = '';
+
+  _Case _targetCase = _Case.upper;
+
+  void copyOutput() {
+    Clipboard.setData(ClipboardData(text: _output));
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Copied to clipboard'),
+      ),
+    );
+  }
+
+  final _caseRegex = RegExp(r'[_\-\s]+');
+
+  String get convertedText {
+    switch (_targetCase) {
+      case _Case.upper:
+        return _input.toUpperCase();
+      case _Case.lower:
+        return _input.toLowerCase();
+      case _Case.camel:
+        return _input
+            // replace large case with space + small case
+            .replaceAllMapped(RegExp(r'[A-Z]'), (match) => ' ${match.group(0)}')
+            .split(_caseRegex)
+            .map((word) => word[0].toUpperCase() + word.substring(1))
+            .join();
+      case _Case.snake:
+        return _input
+            .replaceAllMapped(
+              RegExp(r'[A-Z]'),
+              (match) => '_${match.group(0)}',
+            )
+            .split(_caseRegex)
+            .map(
+              (word) => word.toLowerCase(),
+            )
+            .join('_');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'Enter some text and convert it to the target case',
+          ),
+          onChanged: (value) {
+            setState(() {
+              _input = value;
+              _output = convertedText;
+            });
+          },
+        ),
+        DropdownButton<_Case>(
+          value: _targetCase,
+          onChanged: (value) {
+            setState(() {
+              _targetCase = value!;
+              _output = convertedText;
+            });
+          },
+          items: const [
+            DropdownMenuItem(
+              value: _Case.upper,
+              child: Text('UPPERCASE'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            DropdownMenuItem(
+              value: _Case.lower,
+              child: Text('lowercase'),
+            ),
+            DropdownMenuItem(
+              value: _Case.camel,
+              child: Text('camelCase'),
+            ),
+            DropdownMenuItem(
+              value: _Case.snake,
+              child: Text('snake_case'),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Text(_output),
+        ElevatedButton(
+          onPressed: copyOutput,
+          child: const Text('Copy to clipboard'),
+        ),
+      ],
     );
   }
 }
