8ef9ea506785bd45546f4eb71e061898bbbacb99
adnanjpg <adnanfahedhr@gmail.com>
2023-12-17 01:33:22 +0300

    feat: init password generator

--- lib/features/password_generator/password_generator.dart
+++ lib/features/password_generator/password_generator.dart
@@ -9,7 +9,29 @@ import 'package:remixicon/remixicon.dart';
 enum _PasswordGeneratorMode {
   allChars,
   easyToRead,
-  easyToSay,
+  easyToSay;
+
+  String get name {
+    switch (this) {
+      case _PasswordGeneratorMode.allChars:
+        return 'All Characters';
+      case _PasswordGeneratorMode.easyToRead:
+        return 'Easy to Read';
+      case _PasswordGeneratorMode.easyToSay:
+        return 'Easy to Say';
+    }
+  }
+
+  String get tooltipInfo {
+    switch (this) {
+      case _PasswordGeneratorMode.allChars:
+        return 'Any character combinations like ~!@#\$%^&*()_+-=[]{};:,./<>?';
+      case _PasswordGeneratorMode.easyToRead:
+        return 'Avoid ambiguous characters like 1lI0oO';
+      case _PasswordGeneratorMode.easyToSay:
+        return 'Avoid numbers and special characters';
+    }
+  }
 }
 
 class PasswordGenerator extends StatefulWidget {
@@ -31,6 +53,9 @@ class _PasswordGeneratorState extends State<PasswordGenerator> {
   bool _useNumbers = true;
   bool _useSymbols = true;
 
+  bool _useNumbersIsDisabled = false;
+  bool _useSymbolsIsDisabled = false;
+
   _PasswordGeneratorMode _mode = _PasswordGeneratorMode.allChars;
 
   String _generatePassword() {
@@ -50,6 +75,11 @@ class _PasswordGeneratorState extends State<PasswordGenerator> {
 
     if (_useSymbols) {
       chars.addAll('!@#\$%^&*()_+-=[]{};:,./<>?'.split(''));
+    }
+
+    if (_mode == _PasswordGeneratorMode.easyToRead) {
+      final ambigousChars = '1l0IoOIsS5Z2B8'.split('');
+      chars.removeWhere((char) => ambigousChars.contains(char));
     }
 
     final random = Random();
@@ -114,6 +144,8 @@ class _PasswordGeneratorState extends State<PasswordGenerator> {
     _regeneratePassword();
   }
 
+  bool get isWeak => len < 12;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -129,7 +161,7 @@ class _PasswordGeneratorState extends State<PasswordGenerator> {
             ),
             IconButton(
               onPressed: _regeneratePassword,
-              icon: const Icon(Remix.refresh_fill),
+              icon: const Icon(Remix.refresh_line),
             ),
             IconButton(
               onPressed: () => copyToClipboard(context),
@@ -155,6 +187,7 @@ class _PasswordGeneratorState extends State<PasswordGenerator> {
                 ),
                 Expanded(
                   child: Slider.adaptive(
+                    activeColor: isWeak ? Colors.red[800] : null,
                     value: len.toDouble(),
                     onChanged: (val) => onLenChanged(
                       val.floor().toString(),
@@ -165,6 +198,99 @@ class _PasswordGeneratorState extends State<PasswordGenerator> {
                   ),
                 ),
               ],
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Column(
+                children: _PasswordGeneratorMode.values.map(
+                  (mode) {
+                    final title = mode.name;
+
+                    return RadioListTile<_PasswordGeneratorMode>.adaptive(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(title),
+                          Tooltip(
+                            message: mode.tooltipInfo,
+                            child: const Icon(
+                              Remix.information_line,
+                              size: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      value: mode,
+                      groupValue: _mode,
+                      onChanged: (val) {
+                        if (val == _PasswordGeneratorMode.easyToSay) {
+                          _useSymbols = false;
+                          _useNumbers = false;
+                          _useNumbersIsDisabled = true;
+                          _useSymbolsIsDisabled = true;
+                        } else {
+                          if (val == _PasswordGeneratorMode.allChars) {
+                            _useLowercase = true;
+                            _useUppercase = true;
+                            _useNumbers = true;
+                            _useSymbols = true;
+                          }
+
+                          _useNumbersIsDisabled = false;
+                          _useSymbolsIsDisabled = false;
+                        }
+                        setState(() => _mode = val!);
+                        _regeneratePassword();
+                      },
+                    );
+                  },
+                ).toList(),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  CheckboxListTile.adaptive(
+                    value: _useLowercase,
+                    title: const Text('Lowercase'),
+                    onChanged: (val) {
+                      setState(() => _useLowercase = val!);
+                      _regeneratePassword();
+                    },
+                  ),
+                  CheckboxListTile.adaptive(
+                    value: _useUppercase,
+                    title: const Text('Uppercase'),
+                    onChanged: (val) {
+                      setState(() => _useUppercase = val!);
+                      _regeneratePassword();
+                    },
+                  ),
+                  CheckboxListTile.adaptive(
+                    value: _useNumbers,
+                    title: const Text('Numbers'),
+                    onChanged: _useNumbersIsDisabled
+                        ? null
+                        : (val) {
+                            setState(() => _useNumbers = val!);
+                            _regeneratePassword();
+                          },
+                  ),
+                  CheckboxListTile.adaptive(
+                    value: _useSymbols,
+                    title: const Text('Symbols'),
+                    onChanged: _useSymbolsIsDisabled
+                        ? null
+                        : (val) {
+                            setState(() => _useSymbols = val!);
+                            _regeneratePassword();
+                          },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
